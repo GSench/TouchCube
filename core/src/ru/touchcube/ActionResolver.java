b13b14c0653d65f04ff1a84d3371de43ed383d02
GSench <grishasenchenok@yandex.ru>
2018-08-21 16:11:33 +0300

    starting refactoring; clean project

--- core/src/ru/touchcube/ActionResolver.java
+++
@@ -1,6 +0,0 @@
-package ru.touchcube;
-
-public interface ActionResolver {
-	public void showToast(final String arg0);
-
-}
--- core/src/ru/touchcube/Face.java
+++
@@ -1,8 +0,0 @@
-package ru.touchcube;
-
-import com.badlogic.gdx.graphics.g3d.decals.Decal;
-
-public class Face {
-	public Decal decal;
-	public boolean isDrawing = true;
-}
